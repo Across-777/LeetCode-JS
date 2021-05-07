505f206d7dea5039d14d1d2bc57241962fde2278
wangyanfeng <F201018>
2021-05-07 13:35:17 +0800

    submit [26] 删除有序数组中的重复项

---
+++ 26.删除有序数组中的重复项.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=javascript
+ *
+ * [26] 删除有序数组中的重复项
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0
+  let i = 0, j = 1
+  while (j < nums.length) {
+    if (nums[i] === nums[j]) {
+      j++
+    } else if (nums[i] < nums[j]) {
+      i++
+      nums[i] = nums[j]
+    }
+  }
+  nums.length = i+1
+  return i+1
+};
+// @lc code=end
+
+let a = [1, 1, 2,]
+let b = removeDuplicates(a)
+console.log(a, b);
