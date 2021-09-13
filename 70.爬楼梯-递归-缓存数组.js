040c60baedaacc2583b823b8aa8f7294b46132af
wangyanfeng <1426019383@qq.com>
2021-09-13 17:47:50 +0800

    submit [70] 爬楼梯-递归-缓存数组

---
+++ 70.爬楼梯-递归-缓存数组.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=javascript
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+// 已知的递归终止条件条件
+// n =1   f(n) =1
+// n =2   f(n) =2
+// 数组 下标 0 的位置是 0 是为了占位
+const arr = [0,1,2];
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  if (arr[n]) {
+    return arr[n]
+  }
+  const res = climbStairs(n - 1) + climbStairs(n - 2)
+  arr[n]=res
+  return res
+};
+// @lc code=end
+
+/*
+若是只单纯的迭代则会超时
+因为有很多计算会被重复许多遍
+我们可以用数组，将 n 和计算好的结果 一起存入
+循环时需要用到 n 的结果值可以先查是不是存在，存在就直接用，不存在再计算
+*/
